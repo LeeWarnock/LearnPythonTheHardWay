ea6a71f708bf693fb234c2607c014b0acdf3954b
cmckee <cmckee.dev@gmail.com>
2015-12-27 18:08:06 -0700

    Exercise 1

---
+++ ex1.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# A Good First Program
+
+# print "Hello World!"
+# print "Hello Again"
+# print "I like typing this."
+# print "This is fun."
+# print 'Yay! Printing.'
+# print "I'd much rather you 'not'."
+# print 'I "said" do not touch this.'
+
+print "Valar Morghulis"
+
+# Study drills
+
+# Make your script print another line.
+# Make your script print only one of the lines.
+# Put a # (octothorpe) character at the beginning of a line. What did it do? Try to find out what this character does.
