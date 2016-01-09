a9fea233c0d46a26c1c84a67395896053b255504
cmckee <cmckee.dev@gmail.com>
2016-01-09 16:10:09 -0700

    Exercise 3 and study drill

---
+++ ex3-1.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+# Exercise 3 study drill
+
+car = 446
+insurance = 298
+phone = 113
+
+bills = car + insurance + phone
+
+print "You bills cost $", bills
---
+++ ex3.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python
+# Numbers and Math
+
+# Prints string to screen
+print "I will now count my chickens:"
+# Prints string and does some math
+print "Hens", 25.0 + 30.0 / 6.0
+# Prints string and does some math
+print "Roosters", 100.0 - 25.0 * 3.0 % 4.0
+# Prints String to screen
+print "Now I will count the eggs:"
+# Prints math solution to the screen
+print 3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0
+# Prints string to the screen
+print "Is it true that 3 + 2 < 5 - 7?"
+# Prints math solution to the screen
+print 3.0 + 2.0 < 5.0 - 7.0
+# Prints string and does some math
+print "What is 3 + 2?", 3.0 + 2.0
+# Prints string and does some math
+print "What is 5 + 7?", 5.0 + 7.0
+# Prints sctring to the screen
+print "Oh, that's why it's False."
+# Prints string to the screen
+print "How about some more?"
+# Prints string and does some math
+print "Is it greater?", 5.0 > -2.0
+# Prints string and does some math
+print "Is it greater or equal?", 5.0 >= -2.0
+# Prints string and does some math
+print "Is it less or equal?", 5.0 <= -2.0
+
+# Study drills
+#
+# Above each line, use the # to write a comment to yourself explaining what the line does.
+# Remember in Exercise 0 when you started python? Start python this way again and using the math operators, use Python as a calculator.
+# Find something you need to calculate and write a new .py file that does it.
+# Notice the math seems "wrong"? There are no fractions, only whole numbers. You need to use a "floating point" number, which is a number with a decimal point, as in 10.5, or 0.89, or even 3.0.
+# Rewrite ex3.py to use floating point numbers so it's more accurate. 20.0 is floating point.
