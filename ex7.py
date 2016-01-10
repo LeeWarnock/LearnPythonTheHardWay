d7c8040a2785122621fa4e58fa9c228617d97deb
cmckee <cmckee.dev@gmail.com>
2016-01-09 19:03:55 -0700

    Exercise 7

---
+++ ex7.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python
+#: Author               Curtis Mckee
+#: Description          Learn Python the Hard Way - Exercise 7: More Printing
+#: Email                cmckee.dev@gmail.com
+
+# Prints strings
+print "Mary had a little lamb."
+print "It's fleece was white as %s." % 'snow'
+print "And everywhere that Mary went."
+# Prints 10 .
+print "." * 10 #what'd that do?
+
+# assigns each variable to a letter
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+# Prints out the variables in order
+print end1 + end2 + end3 + end4 + end5 + end6,
+print end7 + end8 + end9 + end10 + end11 + end12
+
+# Study drills
+#
+# Go back through and write a comment on what each line does.
+# Read each one backward or out loud to find your errors.
+# From now on, when you make mistakes, write down on a piece of paper what kind of mistake you made.
+# When you go to the next exercise, look at the mistakes you have made and try not to make them in this new one.
+# Remember that everyone makes mistakes. Programmers are like magicians who fool everyone into thinking they are perfect and never wrong, but it's all an act. They make mistakes all the time.
